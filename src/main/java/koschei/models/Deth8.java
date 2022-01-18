88f54dbdfbb9e2bf0cb3b52a8f242eb75e0c8087
Bolotov Alexander <distalker1@gmail.com>
2022-01-18 08:51:01 +0300

    pom update

--- src/main/java/koschei/models/Deth8.java
+++ src/main/java/koschei/models/Deth8.java
@@ -1,4 +1,11 @@
 package koschei.models;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class Deth8 {
+
+
+
+
 }
--- src/main/java/koschei/models/Needle7.java
+++ src/main/java/koschei/models/Needle7.java
@@ -1,9 +1,25 @@
 package koschei.models;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.Set;
+
+@Component
 public class Needle7 {
+    private Set<Deth8> deth8Set;
+
+
+    public Needle7(Set<Deth8> deth8Set) {
+        this.deth8Set = deth8Set;
+    }
 
     @Override
     public String toString() {
-        return ", смерть Кощея на игле :( " + "";
+        StringBuilder stringBuilder = new StringBuilder(", смерть Кощея на игле :(\n");
+        for (Deth8 deth8 : deth8Set) {
+            stringBuilder.append(deth8.toString()).append("\n");
+        }
+        return stringBuilder.toString();
     }
 }
